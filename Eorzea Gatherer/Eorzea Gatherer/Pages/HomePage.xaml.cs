8decd490f013ce671233559528641a2c89b2752f
Nicholas Martini <nicho.martini@gmail.com>
2019-05-04 19:31:37 +0200

    Added timer to update eorzeaTimeLabel with actual Eorzea Time.

--- Eorzea Gatherer/Eorzea Gatherer/Pages/HomePage.xaml.cs
+++ Eorzea Gatherer/Eorzea Gatherer/Pages/HomePage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,7 +14,7 @@ namespace Eorzea_Gatherer.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
-		public HomePage ()
+		public HomePage()
 		{
 			InitializeComponent ();
 
@@ -28,6 +30,41 @@ namespace Eorzea_Gatherer.Pages
             stack.Children.Add(label2);
 
             Griglia.Children.Add(stack, 6, 0);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //https://xamarinhelp.com/xamarin-forms-timer/
+            //Start the timer that calculates Eorzea Time
+            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () =>
+            {
+                //https://stackoverflow.com/a/7875351/10617365
+                var eorzeaTimeNow = DateTime.Now.ToEorzeaTime().ToString("hh:mm tt", CultureInfo.InvariantCulture);
+                eorzeaTimeLabel.Text = eorzeaTimeNow;
+
+                return true;
+            });
+        }
+    }
+
+    public static class Extension
+    {
+        public static DateTime ToEorzeaTime(this DateTime date)
+        {
+            //https://www.reddit.com/r/ffxiv/comments/2pbl8p/eorzea_time_formula/cmvijkz?utm_source=share&utm_medium=web2x
+            //https://olitee.com/2015/01/c-convert-current-time-ffxivs-eorzea-time/
+
+            const double EorzeaMultiplier = 3600d / 175d;
+
+            // Calculate how many ticks have elapsed since 1/1/1970
+            long epochTicks = date.ToUniversalTime().Ticks - (new DateTime(1970, 1, 1).Ticks);
+
+            // Multiply those ticks by the Eorzea multipler (approx 20.5x)
+            long eorzeaTicks = (long)Math.Round(epochTicks * EorzeaMultiplier);
+
+            return new DateTime(eorzeaTicks);
         }
     }
 }
