3268c85ea99196eb090919726a049cbf5cf1db6b
Nicholas Martini <nicho.martini@gmail.com>
2019-05-05 11:17:31 +0200

    Added Nodes class.

---
+++ Eorzea Gatherer/Eorzea Gatherer/Nodes.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Eorzea_Gatherer
+{
+    class Nodes
+    {
+        #region JSON
+        public class Reduce
+        {
+            public string item { get; set; }
+            public int icon { get; set; }
+        }
+
+        public class Item
+        {
+            public string item { get; set; }
+            public int icon { get; set; }
+            public int id { get; set; }
+            public string slot { get; set; }
+            public string scrip { get; set; }
+            public Reduce reduce { get; set; }
+        }
+
+        public class Node
+        {
+            public string type { get; set; }
+            public string func { get; set; }
+            public List<Item> items { get; set; }
+            public int stars { get; set; }
+            public List<int> time { get; set; }
+            public string title { get; set; }
+            public string zone { get; set; }
+            public List<int> coords { get; set; }
+            public string name { get; set; }
+            public int uptime { get; set; }
+            public int lvl { get; set; }
+            public int id { get; set; }
+            public double patch { get; set; }
+            public string condition { get; set; }
+            public string bonus { get; set; }
+        }
+        #endregion
+
+        #region Methods
+        //Get the nodes from the JSON file and return a list of nodes
+        public static List<Node> GetNodes()
+        {
+            using (StreamReader streamReader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "Nodes.json"))
+            {
+                string s = streamReader.ReadToEnd();
+                List<Node> nodes = JsonConvert.DeserializeObject<List<Node>>(s);
+
+                return nodes;
+            }
+        }
+
+        //Compiles a list of gatherable items from the nodes list
+        public static List<Nodes.Item> GetUniqueItems(List<Node> nodes)
+        {
+            List<Nodes.Item> uniqueItems = new List<Item>();
+
+            foreach (var node in nodes)
+            {
+                foreach (var item in node.items)
+                {
+                    //https://stackoverflow.com/a/2629303/10617365
+                    if (!uniqueItems.Any(x => x.id == item.id))
+                    {
+                        uniqueItems.Add(item);
+                    }
+                }
+            }
+
+            return uniqueItems;
+        }
+        #endregion
+    }
+}
