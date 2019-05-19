73a005b666360c9b77e07e286283069ed8ee2ecf
Nicholas Martini <nicho.martini@gmail.com>
2019-05-19 10:39:23 +0200

    Added code for removing items from the tracking list.

--- Eorzea Gatherer/Eorzea Gatherer/Pages/TrackingPage.xaml.cs
+++ Eorzea Gatherer/Eorzea Gatherer/Pages/TrackingPage.xaml.cs
@@ -25,5 +25,17 @@ namespace Eorzea_Gatherer.Pages
             trackingListView.ItemsSource = null;
             trackingListView.ItemsSource = Nodes.TrackingList.Items;
         }
+
+        #region Events
+        public void RemoveTracking(object sender, EventArgs e)
+        {
+            //Retrieve the object bound to the cell raising the event
+            var mi = (MenuItem)sender;
+            var item = (Nodes.Item)mi.BindingContext;
+
+            //Remove node to the tracking list view
+            Nodes.TrackingList.Items.Remove(item);
+        }
+        #endregion
     }
 }
