a1de9414d52390356c634b9689e4a977c71bac8d
Nicholas Martini <nicho.martini@gmail.com>
2019-05-23 21:06:58 +0200

    Created TrackingList model class to replace part of Nodes model class.

---
+++ Eorzea Gatherer/Eorzea Gatherer/Models/TrackingList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Eorzea_Gatherer.Models
+{
+    public class TrackingList
+    {
+        public static ObservableCollection<Item> Items { get; set; }
+
+        static TrackingList()
+        {
+            Items = new ObservableCollection<Item>();
+        }
+    }
+}
